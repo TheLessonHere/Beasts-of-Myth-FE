95f503f54f41439a4eea07dc93df92cc7ac56e00
TheLessonHere <48539448+TheLessonHere@users.noreply.github.com>
2019-11-27 18:46:28 -0700

    Added dependencies and router for root path

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+// Dependencies
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+// Components
+import Dashboard from './views/dashboard';
+// Styling
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>WELCOME TO BEASTS OF MYTH</h1>
-    </div>
+    <Router>
+      <Route exact path="/" component={Dashboard} />
+    </Router>
   );
 }
 
