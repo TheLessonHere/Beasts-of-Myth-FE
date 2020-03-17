ad0a30ee51f223abbafed5301095ca9c02273bcf
TheLessonHere <48539448+TheLessonHere@users.noreply.github.com>
2020-03-17 15:46:15 -0600

    Updated test data and seperated it into library files

---
+++ src/views/teambuilder/libraries/AbilityLibrary.js
@@ -0,0 +1,14 @@
+const abilities = [
+    {
+        data_type: "Ability",
+        ability_name: "Power-Up",
+        search_id: "power-up"
+    },
+    {
+        data_type: "Ability",
+        ability_name: "Turnabout",
+        search_id: "turnabout"
+    }
+];
+
+export { abilities };
---
+++ src/views/teambuilder/libraries/BeastLibrary.js
@@ -0,0 +1,19 @@
+const beasts = [
+    {
+        data_type: "Beast",
+        beast_name: "Jessie",
+        search_id: "jessie"
+    },
+    {
+        data_type: "Beast",
+        beast_name: "James",
+        search_id: "james"
+    },
+    {
+        data_type: "Beast",
+        beast_name: "Meowth",
+        search_id: "meowth"
+    }
+];
+
+export { beasts };
